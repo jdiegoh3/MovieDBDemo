9fdb192c4e09a90212bed13b7716892e3247f758
Juan Diego H <jdiegoh3@gmail.com>
2019-08-21 23:59:56 -0500

    Added internet permission and created static constants class

---
+++ app/src/main/java/com/example/moviesdbdemo/utils/StaticConstants.java
@@ -0,0 +1,8 @@
+package com.example.moviesdbdemo.utils;
+
+public class StaticConstants {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String API_KEY = "b24b323885d7efaa27e0159c5764ab7d";
+
+}
