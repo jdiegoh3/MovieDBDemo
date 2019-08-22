2a7f8db093a38a20a8b560466f2d4476ae7fa625
Juan Diego H <jdiegoh3@gmail.com>
2019-08-21 23:54:17 -0500

    Created BaseActivity extending from AppCompatActivity

---
+++ app/src/main/java/com/example/moviesdbdemo/ui/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.example.moviesdbdemo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/moviesdbdemo/ui/movies_list/MovieListActivity.java
+++ app/src/main/java/com/example/moviesdbdemo/ui/movies_list/MovieListActivity.java
@@ -1,10 +1,13 @@
-package com.example.moviesdbdemo;
+package com.example.moviesdbdemo.ui.movies_list;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MovieListActivity extends AppCompatActivity {
+import com.example.moviesdbdemo.R;
+import com.example.moviesdbdemo.ui.BaseActivity;
+
+public class MovieListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
